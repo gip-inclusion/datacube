11597dd043a1f8abd2d65b5fdc0baa37828d8083
Victor Perron <victor@iso3103.net>
2024-10-08 10:35:39 +0200

    maj scaleway plugins

--- deployment/provider.tf
+++ deployment/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.30.0"
+      version = "2.44.0"
     }
     time = {
       source  = "hashicorp/time"
